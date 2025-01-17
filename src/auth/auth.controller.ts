feb3e055c3ab04e7adaae7c22543599a16e87ca5
craig <craigtipple81@gmail.com>
2025-01-17 13:50:06 +0000

    Added auth controller and auth service. Created barebones register and signup using email and password. Installed ConfigService and implemented in auth.service.ts and app.module.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  // Create a constructor that uses the authService
+  constructor(private readonly authService: AuthService) {}
+
+  // Define the REGISTER route
+  @Post('register')
+  async register(@Body() body: { email: string; password: string }) {
+    return this.authService.register(body.email, body.password);
+  }
+
+  // Define the LOGIN route
+  @Post('signin')
+  async signin(@Body() body: { email: string; password: string }) {
+    return this.authService.signin(body.email, body.password);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  // Instantiate the SupabaseClient
+  private supabase: SupabaseClient;
+
+  // Create a constructor that initialises the supabaseClient with our credentials
+  constructor(private readonly configService: ConfigService) {
+    const supabaseUrl = this.configService.get<string>('SUPABASE_URL');
+    const supabasekey = this.configService.get<string>(
+      'SUPABASE_SERVICE_ROLE_KEY',
+    );
+
+    this.supabase = createClient(supabaseUrl, supabasekey);
+  }
+
+  async register(email: string, password: string) {
+    const { data, error } = await this.supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) throw new Error(error.message);
+
+    return data;
+  }
+
+  async signin(email: string, password: string) {
+    const { data, error } = await this.supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) throw new Error(error.message);
+
+    return data;
+  }
+}
